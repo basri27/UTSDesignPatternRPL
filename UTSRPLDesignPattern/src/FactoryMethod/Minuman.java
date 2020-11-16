5af3af85eec3577f1e411e2a224520b9363f2133
hutanja <hutan17hijau@gmail.com>
2020-11-16 14:02:18 -0800

    Menambahkan class Minuman.java sebagai produk yang methodnya akan diimplementasikan pada concrate produk

--- UTSRPLDesignPattern/src/FactoryMethod/Minuman.java
+++ UTSRPLDesignPattern/src/FactoryMethod/Minuman.java
@@ -2,8 +2,7 @@ package FactoryMethod;
 
 public interface Minuman{
     void rasa();
-    int a;
 }
 
-//digunakan sebagai produk yang methodnya akan diimplementasikan pada concrate produk
-//concrate produk berupa Milkshake.java-FruityLemonSquash.java-HoneyCitrunIced.java
+/*digunakan sebagai produk yang methodnya akan diimplementasikan pada concrate produk
+concrate produk berupa Milkshake.java-FruityLemonSquash.java-HoneyCitrunIced.java*/
