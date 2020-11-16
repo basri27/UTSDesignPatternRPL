db47c0662a6fde14635551068443ab3a3ef7a4a9
hutanja <hutan17hijau@gmail.com>
2020-11-17 06:21:04 +0800

    Modifikasi class Main.java untuk memanggil template method dan menjalankan fungsi methodnya

--- UTSRPLDesignPattern/src/Main.java
+++ UTSRPLDesignPattern/src/Main.java
@@ -1,5 +1,13 @@
+import TemplateMethod.*;
+
 public class Main {
-    public static void main(String[]args ){
+    public static void main(String[] args) {
         
+        TemplateMakanan makanan = new TemplateMakanan();
+        makanan.start();
+        
+        TemplateMinuman minuman = new TemplateMinuman();
+        minuman.start();
     }
+    
 }
