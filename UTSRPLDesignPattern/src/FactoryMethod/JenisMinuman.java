09fa2f6f9a24391bcee20ef188a3616161832bca
hutanja <hutan17hijau@gmail.com>
2020-11-16 14:04:01 -0800

    Menambahkan class JenisMinuman.java sebagai tempat menyimpan static variabel untuk menyimpan nilai atau value dari method getMinuman

---
+++ UTSRPLDesignPattern/src/FactoryMethod/JenisMinuman.java
@@ -0,0 +1,9 @@
+package FactoryMethod;
+
+public class JenisMinuman{
+    public static final String MILK_SHAKE = "Milk Shake";
+    public static final String FRUITY_LEMON_SQUASH = "Fruity Lemon Squash";
+    public static final String HONEY_CITRON_ICED = "Honey Citron Iced";
+}
+
+// Menyimpan static variable untuk menyimpan nilai atau value dari Method getMinuman ,yang berperan sebagi Concrete Creator.
